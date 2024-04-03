eb0f0380fef4b907c33efd997dbfde81c9445bf3
William Mendes <william.costa@softsy.com.br>
2024-04-03 08:02:30 -0300

    navegação de página

--- src/Cadastro.jsx
+++ src/Cadastro.jsx
@@ -1,33 +1,44 @@
 import { Link } from 'react-router-dom'
 import './Cadastro.css'
+import { useState } from 'react'
+
 
 function Cadastro(){
 
+    const [usuario, setUsuario] = useState()
+    const [senha, setSenha] = useState()
+
+    const armazenar = (chave1,chave2,valor1,valor2)=>{
+        localStorage.setItem(chave1, valor1)
+        localStorage.setItem(chave2, valor2)
+    }
+
+  
     return(
         <>
             <div className='container-cadastro'>
 
-            <img className='circulo' src="https://media.discordapp.net/attachments/1179185738013757466/1213719044297990214/circulo.png?ex=65f67ef1&is=65e409f1&hm=7c7041a06264df33ea37dff9c5c50400d481f43d040e5c9d1e9cb14d0366da7b&=&format=webp&quality=lossless&width=173&height=480" alt="circulo" />
+            <img className='circulo' src="https://media.discordapp.net/attachments/1179185738013757466/1213719044297990214/circulo.png?ex=661b68f1&is=6608f3f1&hm=e4f97fed8d1409f6b140093b1b0052d7c7de9f3f94a918698a7ab77b1f34c664&=&format=webp&quality=lossless&width=243&height=673" alt="circulo" />
 
                 <form action="">
 
-                <h1 className='titulo'>Cadastro</h1>
+                <h1 className='titulo'>{usuario}</h1>
                 <div className='container-inputs'>
 
-                <input type="text" name="usuario" id="usuario" placeholder='Usuário' required/>
-                <input type="password" name="password" id="senha" placeholder='Senha' required/>
+                <input type="text" value={usuario} onChange={(e)=>setUsuario(e.target.value)} name="usuario" id="usuario" placeholder='Usuário' required/>
+                <input type="password" value={senha} onChange={(e)=>setSenha(e.target.value)} name="password" id="senha" placeholder='Senha' required/>
                 <input type="email" name="email" id="email" placeholder='E-mail' required/>
                 <input type="text" name="nome" id='nome' placeholder='Nome do Jogador' required/>
-                <button className='botao-entrar'>Cadastrar</button>
+                <button className='botao-entrar' onClick={()=>armazenar("usuario","senha",usuario,senha)}>Cadastrar</button>
                 <p className='texto-informacao'>Já tem cadastro? Faça Login <Link className='chama-cadastro' to={"/"}>AQUI</Link></p>
 
                 </div>
 
                 </form>
 
-                <img className='listras' src="https://media.discordapp.net/attachments/1179185738013757466/1213719044734320690/listras.png?ex=65f67ef2&is=65e409f2&hm=69630d6bd610bb30760ea602ea9d3020dae4c6a82250c764047c60e78ad33009&=&format=webp&quality=lossless" alt="listras" />
+                <img className='listras' src="https://media.discordapp.net/attachments/1179185738013757466/1213719044734320690/listras.png?ex=661b68f2&is=6608f3f2&hm=3b8cc954d079c090ded6520efe406b7cbbb62b895ed7b1e9ce2c15eb21c938bd&=&format=webp&quality=lossless&width=676&height=528" alt="listras" />
        
-        <img className='triangulos' src="https://media.discordapp.net/attachments/1179185738013757466/1213719044520153098/triangulos.png?ex=65f67ef2&is=65e409f2&hm=bba4cd78b49d32f8dbbbd7bf0b67e14646983fbfdd31cdb68c708a2d9bb842fe&=&format=webp&quality=lossless" alt="triangulos" />
+        <img className='triangulos' src="https://media.discordapp.net/attachments/1179185738013757466/1213719044520153098/triangulos.png?ex=661b68f2&is=6608f3f2&hm=66ac805ab5ed7144e1816811a869f3e6596518cd0c9817176a39b779152a333e&=&format=webp&quality=lossless&width=720&height=648" alt="triangulos" />
 
             </div>
         
